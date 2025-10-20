6ee48f99c665eb87f7bf89e1cf63468b98e520f7
Trevor Trinh <50549133+trevor-trinh@users.noreply.github.com>
2025-10-20 11:59:31 -0400

    clickhouse schema

---
+++ apps/backend/src/db/clickhouse/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS candles (
+    market_id TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    open BIGINT NOT NULL,
+    high BIGINT NOT NULL,
+    low BIGINT NOT NULL,
+    close BIGINT NOT NULL,
+    volume BIGINT NOT NULL,
+    PRIMARY KEY (market_id, timestamp)
+);
